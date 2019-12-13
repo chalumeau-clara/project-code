d3df997752709a8e85245c402f31fa717ab8c004
chalumeau-clara <58848669+chalumeau-clara@users.noreply.github.com>
2019-12-13 12:15:48 +0100

    Add files via upload

---
+++ break_ciphers.ml
@@ -0,0 +1,9 @@
+(** Factoring Built-In Int Primes *)
+
+open Builtin
+open Basic_arithmetics
+
+(** Factors product of two primes.
+    @param key is public key of an RSA cryptosystem.
+ *)
+let break key = (0, 0)
