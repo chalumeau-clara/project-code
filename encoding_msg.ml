dac82209cdd4783ec7da55c40c741ba62c9ac1d0
chalumeau-clara <58848669+chalumeau-clara@users.noreply.github.com>
2019-12-13 12:16:40 +0100

    Add files via upload

---
+++ encoding_msg.ml
@@ -0,0 +1,19 @@
+(** Encoding Strings *)
+
+open Builtin
+open Basic_arithmetics
+open Power
+
+(** Encode a string containing ASCII characters.
+    @param str is a string representing message.
+    @param bits number of bits on which to store a character ;
+           alphanumeric ASCII is 7.
+ *)
+let encode str bits = 0
+
+(** Decode a string containing ASCII characters.
+    @param msg is an integer representing an encoded message.
+    @param bits number of bits on which to store a character ;
+           alphanumeric ASCII is 7.
+ *)
+let decode msg bits = ""
