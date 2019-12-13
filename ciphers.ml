0d83ce23438e72027e416dc6ab448f375f84c6d8
chalumeau-clara <58848669+chalumeau-clara@users.noreply.github.com>
2019-12-13 12:16:25 +0100

    Add files via upload

---
+++ ciphers.ml
@@ -0,0 +1,97 @@
+(** Ciphers
+    Built-in integer based ciphers.
+*)
+
+open Builtin
+open Basic_arithmetics
+open Power
+open Generate_primes
+(********** Cesar Cipher **********)
+
+(** Cesar's cipher encryption
+    @param k is an integer corresponding to key
+    @param m word to cipher.
+    @param b base ; for ASCII codes should be set to 255.
+ *)
+let rec encrypt_cesar k m b =
+  if m = [] then invalid_arg " la liste ne doit pas être vide"
+  else let rec encesar = function
+     [] -> []
+    |e::l-> modulo (k+e) b :: encesar l
+       in encesar m;;
+
+
+
+(** Cesar's cipher decryption
+    @param k is an integer corresponding to key
+    @param m encrypted word.
+    @param b base ; for ASCII code should be set to 255.
+ *)
+let decrypt_cesar k m b =
+    if m = [] then invalid_arg " la liste ne doit pas être vide"
+  else let rec encesar = function
+     [] -> []
+    |e::l-> modulo (e-k) b :: encesar l
+       in encesar m;;
+
+(********** RSA Cipher **********)
+
+(** Generate an RSA ciphering keys.
+    Involved prime numbers need to be distinct. Output is a couple
+    of public, private keys.
+    @param p prime number
+    @param q prime number
+*)
+ let generate_keys_rsa p q =
+  let n = p*q in
+  let phi = (p-1)*(q-1) in
+  let f = let rec fonction = function
+    []-> 1
+    |e::l -> if (modulo phi e) = 1 then e
+      else fonction l
+  in fonction (eratosthenes phi)
+  in let d = let rec fonc d =
+if (modulo (d * f) phi) = 1 then d else fonc (d+1)in fonc 0
+in (n,f),(d,n);;
+
+(** Encryption using RSA cryptosystem.
+    @param m integer hash of message
+    @param pub_key a tuple (n, e) composing public key of RSA cryptosystem.
+ *)
+let encrypt_rsa m (n, e) =
+  (modulo (power m e) n);;
+
+(** Decryption using RSA cryptosystem.
+    @param m integer hash of encrypter message.
+    @param pub_key a tuple (n, d) composing private key of RSA cryptosystem.
+ *)
+let decrypt_rsa m (n , d) =
+  (modulo (power m d) n);;
+;;
+
+(********** ElGamal Cipher **********)
+
+(** Generate ElGamal public data. Generates a couple (g, p)
+    where p is prime and g having high enough order modulo p.
+    @param p is prime having form 2*q + 1 for prime q.
+ *)
+let rec public_data_g p = (0, 0)
+
+(** Generate ElGamal public data.
+    @param pub_data a tuple (g, p) of public data for ElGamal cryptosystem.
+ *)
+let generate_keys_g (g, p) = (0, 0)
+
+(** ElGamal encryption process.
+    @param msg message to be encrypted.
+    @param pub_data a tuple (g, p) of ElGamal public data.
+    @param kA ElGamal public key.
+ *)
+let encrypt_g msg (g, p) kA = (0, 0)
+
+(** ElGamal decryption process.
+    @param msg a tuple (msgA, msgB) forming an encrypted ElGamal message.
+    @param a private key
+    @param pub_data a tuple (g, p) of public data for ElGamal cryptosystem.
+ *)
+let decrypt_g (msgA, msgB) a (g, p) = 0
