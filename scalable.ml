1367d95d069e42873f13c32b2d44412eafb17fe1
chalumeau-clara <58848669+chalumeau-clara@users.noreply.github.com>
2019-12-17 13:57:45 +0100

    Add files via upload

--- scalable.ml
+++ scalable.ml
@@ -18,20 +18,24 @@ decomposition of a non-negative integer.
     @param x built-in integer.
 *)
 
-let inverse_n l =
+let inverse_n p =
   let rec f d = function
 [] -> d
-    |e::l -> f (e::d) l
-  in f [] l;;
+    |e::l -> f (e::p) l
+  in f [] p;;
 
-let inverse_b l =
-  let rec f d = function
-[] -> d
-    |e::l -> f (e::d) l
+let inverse_b p =
+  let rec fi d = function
+  [] -> d
+    |e::l -> fi (e::d) l
   in
-  match l with
+  match p with
       [] -> []
-    |e::l -> e:: (f l);;
+    |e::l -> e:: (fi [] l);;
+
+let rec length = function
+[]-> 0
+  |e::l -> 1+ length l;;
 
 let from_int x = if x = 0 then [] else let y = x in 
   let rec dectobin = function
@@ -71,6 +75,8 @@ let print_b bA =
     |e::bA ->  print_string "0" ;
       print_string (print "" bA) ;;
 
+
+
 let rec compare_n nA nB =
   let a = inverse_n nA in 
   let b = inverse_n nB in
@@ -80,8 +86,9 @@ let rec compare_n nA nB =
     |([], _::_) -> -1
     |(e::l,f::g) when e = f -> comp (l,g)
     |(e::l,f::g) when e > f -> 1
-    |(e::l,f::g) when e < f -> -1
-  in comp (a,b);;
+    |(e::l,f::g)  -> -1
+  in
+  if (length nA) > (length nB) then 1 else (if (length nA) < (length nB) then -1 else comp (a,b));;
 
 (** Bigger inorder comparison operator on naturals. Returns true if
     first argument is bigger than second and false otherwise.
@@ -125,22 +132,31 @@ let (<=!) nA nB =
     @param bA A bitarray.
     @param bB A bitarray.
 *)
+
+let rec length = function
+[]-> 0
+  |e::l -> 1+ length l;;
+
 let compare_b bA bB =
-  match (bA,bB) with
-      ([],[]) -> 0
+   let a = inverse_b bA in 
+  let b = inverse_b bB in
+  let rec comp = function 
+  ([],[]) -> 0
     |(_::_, []) -> 1
     |([], _::_) -> -1
-    |(e::l,f::g) -> if (<<!) l g  then -1 else (if (>>!) l g then 1 else 0);;
-
+    |(e::l,f::g) when e = f -> comp (l,g)
+    |(e::l,f::g) when e > f -> 1
+    |(e::l,f::g)  -> -1
+  in
+if (length bA) > (length bB) then 1 else (if (length bA) < (length bB) then -1 else comp(a,b));;
+  
 (** Bigger inorder comparison operator on bitarrays. Returns true if
     first argument is bigger than second and false otherwise.
     @param nA natural.
     @param nB natural.
  *)
 let (<<) bA bB =
-   let a = to_int nA in
-  let b = to_int nB in
-  if a >  b then true else false ;;
+   if compare_b bA bB = 1 then true else false;;
 
 (** Smaller inorder comparison operator on bitarrays. Returns true if
     first argument is smaller than second and false otherwise.
@@ -148,10 +164,7 @@ let (<<) bA bB =
     @param nB natural.
  *)
 let (>>) bA bB =
-  let a = to_int nA in
-  let b = to_int nB in
-  if a >= b then true else false ;;
-
+ if compare_b bA bB = 1 then true else false;;
 (** Bigger or equal inorder comparison operator on bitarrays. Returns
     true if first argument is bigger or equal to second and false
     otherwise.
@@ -159,9 +172,7 @@ let (>>) bA bB =
     @param nB natural.
  *)
 let (<<=) bA bB =
-  let a = to_int nA in
-  let b = to_int nB in
-  if a <=  b then true else false ;;
+ if compare_b bA bB = 1 then false else true;;
 
 (** Smaller or equal inorder comparison operator on naturals. Returns
     true if first argument is smaller or equal to second and false
@@ -170,9 +181,7 @@ let (<<=) bA bB =
     @param nB natural.
  *)
 let (>>=) bA bB =
-  let a = to_int nA in
-  let b = to_int nB in
-  if a >= b then true else false ;;
+  if compare_b bA bB = (-1) then false else true;;
 
 (** Sign of a bitarray.
     @param bA Bitarray.
@@ -211,7 +220,15 @@ let _div_t a = (0, 0)
     @param nA Natural.
     @param nB Natural.
 *)
-let add_n nA nB = []
+let add_n nA nB =
+  let rec add d= function 
+  ([],[]) -> []
+    |(e::l, []) |([], _::_) -> e:: add 0 (l,g)
+    |(e::l,f::g) when (e = f && e = 0) -> d :: add 0 (l,g) 
+    |(e::l,f::g) when (e = f && e = 1) -> 0:: add 1 (l,g)
+    |(e::l,f::g) when e = 1 -> 1:: add 0 (l,g)
+    |(e::l,f::g)  -> 1:: add 0 (l,g)
+  in add 0 (nA,nB);;
 
 (** Difference of two naturals.
     UNSAFE: First entry is assumed to be bigger than second.
